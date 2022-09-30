c9a747205a0a9a3edadc908c4c91fd0a2604ef7f
MD Tahseen Equbal <92607061+Md-Tahseen-Equbal@users.noreply.github.com>
2022-09-30 16:12:42 +0530

    Update Calculator.java

--- Lab-Session/29-09-2022/Calculator.java
+++ Lab-Session/29-09-2022/Calculator.java
@@ -1,3 +1,7 @@
+//Ques1:create a class parent with default constructor and void sing method.print something inside the constructor and method
+//Create child class which will inherit the property of parent and has methods void speak and void dance.print something inside the two methods.
+//Create another class inheritance and object of class and call all the methods.also mention the type of inheritance on the top of the program.
+
 package Lab;
 import java.util.Scanner;
 class Calculator 
