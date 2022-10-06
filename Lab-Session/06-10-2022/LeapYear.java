c491347aaf33ac532e74bc45fc497e6ed3884ce2
MD Tahseen Equbal <92607061+Md-Tahseen-Equbal@users.noreply.github.com>
2022-10-06 14:59:42 +0530

    Add files via upload

---
+++ Lab-Session/06-10-2022/LeapYear.java
@@ -0,0 +1,32 @@
+
+// Q1 - WAP to Check for Leap Year.Take the input from the user
+
+package Lab;
+
+import java.util.Scanner;
+public class LeapYear {
+   public static void main(String[] args){
+	   
+      int year;
+      
+      System.out.println("Enter an Year :: ");
+      
+    //Creating Scanner object to take input from user 
+      
+      Scanner sc = new Scanner(System.in);
+      year = sc.nextInt();
+      
+      //Using If-Else Condition With && And || Operator for Verify Leap year
+      
+      // If is used to verify Given Condition is True then print Leap Year
+
+      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+         System.out.println("Given year is a leap year");
+      
+      //Else Is used to print Not Leap year when Given Condition is False.
+      
+      else
+         System.out.println("Given year is not a leap year");
+   }
+}
+
