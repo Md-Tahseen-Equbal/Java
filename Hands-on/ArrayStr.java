bcbf9c4423a8027845c3c876b358d286b9dc7fa9
MD Tahseen Equbal <92607061+Md-Tahseen-Equbal@users.noreply.github.com>
2022-10-19 15:15:31 +0530

    Add files via upload

---
+++ Hands-on/ArrayStr.java
@@ -0,0 +1,20 @@
+package Corejava;
+
+public class ArrayStr {
+
+	public static void main(String[] args) {
+       char a[]= {'a','b','c'};
+		
+		//Initialization
+		
+		
+		
+		//Traversing Array
+		
+		for(char i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+
+	}
+
+}
+}
